6cef63c60606a6bd8be5552a6cb17cca1384f716
Diarilala <hei.diarilala@gmail.com>
2026-03-30 14:16:50 +0300

    feat: add spring-boot initialization

---
+++ src/main/java/td/spring_boot/ingredient_spring_boot/IngredientSpringBootApplication.java
@@ -0,0 +1,13 @@
+package td.spring_boot.ingredient_spring_boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IngredientSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IngredientSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/td/spring_boot/ingredient_spring_boot/IngredientSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package td.spring_boot.ingredient_spring_boot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IngredientSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
